edad8992f92d81205051999b0089b8781b96491d
Nathan Bell <nathan.bell@mail.utoronto.ca>
2025-08-28 11:42:25 -0600

    Implemented config file

--- loan_credit_analysis/config.py
+++ loan_credit_analysis/config.py
@@ -3,7 +3,7 @@ import os
 # Project root directory
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-# Data folder and database path
+# Data directory and database path
 DB_DIR = os.path.join(ROOT_DIR, "data")
 DB_PATH = os.path.join(DB_DIR, "loan_portfolio.db")
 
